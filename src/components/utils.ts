f5000fc4a6792f7e509840fdbdca67e74375cba4
jegatheesprana <jegatheesprana@gmail.com>
2025-01-11 11:17:41 +0530

    modified the intentation

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -5,19 +5,19 @@ import { TaskDragData } from "./TaskCard";
 type DraggableData = ColumnDragData | TaskDragData;
 
 export function hasDraggableData<T extends Active | Over>(
-  entry: T | null | undefined
+    entry: T | null | undefined
 ): entry is T & {
-  data: DataRef<DraggableData>;
+    data: DataRef<DraggableData>;
 } {
-  if (!entry) {
+    if (!entry) {
+        return false;
+    }
+
+    const data = entry.data.current;
+
+    if (data?.type === "Column" || data?.type === "Task") {
+        return true;
+    }
+
     return false;
-  }
-
-  const data = entry.data.current;
-
-  if (data?.type === "Column" || data?.type === "Task") {
-    return true;
-  }
-
-  return false;
 }
