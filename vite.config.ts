814b668a2ac680486203501ee3b17e82184b9ffb
jegatheesprana <jegatheesprana@gmail.com>
2025-01-13 01:06:10 +0530

    fixed vite path issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-  base: mode === "production" ? "/react-dnd-kit-tailwind-shadcn-ui/" : "/",
+    base: mode === "production" ? "/" : "/",
 }));
